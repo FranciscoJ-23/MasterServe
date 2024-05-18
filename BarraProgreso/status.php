5413780adb0035a57e3c98064bf94cca849f7baa
cisco p <55768594+FranciscoPG-69@users.noreply.github.com>
2024-05-18 17:30:41 -0600

    Filtrado por busqueda de palabras y modificaciones a botones en lista

--- BarraProgreso/status.php
+++ BarraProgreso/status.php
@@ -27,7 +27,7 @@ if (!$solicitud) {
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="jquery-3.6.0.min.js"></script>
-    <title>Progress Bar</title>
+    <title>Progreso de solicitud</title>
 </head>
 <body>
     <div> <!--Progress-->
--- php/lista.php
+++ php/lista.php
@@ -1,6 +1,8 @@
 <?php
-include  "conexion_be.php";
+include "conexion_be.php";
+session_start();
 ?>
+
 <!doctype html>
 <html lang="en">
 
@@ -14,6 +16,10 @@ include  "conexion_be.php";
             max-width: 1000px;
             margin: 0 auto;
             padding: 0 20px;
+        }
+        .button-right{
+            text-align: right;
+            margin-bottom: 20px;
         }
 
         .text-center {
@@ -77,39 +83,61 @@ include  "conexion_be.php";
 <body>
     <div class="container">
         <h1 class="text-center">Lista de servicios</h1>
+        <div class="button-right">
+            <?php
+            // Mostrar el botón de creación si el usuario ha iniciado sesión como servidor
+            if (isset($_SESSION['usuario_correo'])) {
+                echo '<button onclick="window.location.href=\'registrar_public.php\'">';
+                echo '<span>';
+                echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path></svg> Crear';
+                echo '</span>';
+                echo '</button>';
+            }
+            ?>
+        </div>
+        <div class="search-form">
+            <form action="lista.php" method="GET">
+                <input type="text" name="keyword" placeholder="Buscar por palabra clave">
+                <button type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15.7 13.3l-3.81-3.83A5.93 5.93 0 0 0 13 6c0-3.31-2.69-6-6-6S1 2.69 1 6s2.69 6 6 6c1.3 0 2.48-.41 3.47-1.11l3.83 3.81c.19.2.45.3.7.3.25 0 .52-.09.7-.3a.996.996 0 0 0 0-1.41v.01zM7 10.7c-2.59 0-4.7-2.11-4.7-4.7 0-2.59 2.11-4.7 4.7-4.7 2.59 0 4.7 2.11 4.7 4.7 0 2.59-2.11 4.7-4.7 4.7z"/></svg>
+                </button>
+            </form>
+        </div>
         <?php
+        // Obtener la palabra clave de la URL si está presente
+        $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+
+        // Construir la consulta SQL basada en la palabra clave
         $sql = "SELECT p.*, u.nombre_completo AS nombre_usuario 
                 FROM publicaciones p 
                 INNER JOIN usuarios u ON p.id_nombre_completo = u.id";
+
+        if (!empty($keyword)) {
+            // Agregar condición WHERE para filtrar por palabra clave
+            $sql .= " WHERE titulo LIKE '%$keyword%' OR descripcion LIKE '%$keyword%'";
+        }
+
         $resultado = $conexion->query($sql);
         while ($datos = $resultado->fetch_object()) { ?>
             <div class="service-container">
                 <div class="service-details">
-                    <h2><?= $datos->titulo ?></h2>
-                    <p><?= $datos->descripcion ?></p>
-                    <p><?= $datos->nombre_usuario ?></p>
-                    <p><?= $datos->telefono ?></p>
+                    <h2><?= htmlspecialchars($datos->titulo) ?></h2>
+                    <p><?= htmlspecialchars($datos->descripcion) ?></p>
+                    <p><?= htmlspecialchars($datos->nombre_usuario) ?></p>
+                    <p><?= htmlspecialchars($datos->telefono) ?></p>
                 </div>
                 <div class="service-actions">
-                    <a href="enviodecorreo.php?id_publicacion=<?= $datos->Id ?>">
-                        <button>Solicitar</button>
-                    </a>
+                    <?php
+                    // Ocultar el botón de solicitar si el usuario ha iniciado sesión como servidor
+                    if (!isset($_SESSION['usuario_correo'])) {
+                        echo '<a href="enviodecorreo.php?id_publicacion=' . htmlspecialchars($datos->Id) . '">';
+                        echo '<button>Solicitar</button>';
+                        echo '</a>';
+                    }
+                    ?>
                 </div>
-
             </div>
         <?php }
-        ?>
-        <?php
-        // Verificar si el usuario ha iniciado sesión y así mostrar el botón de creación 
-        session_start();
-        if (isset($_SESSION['usuario_correo'])) {
-            // Mostrar el botón solo si el usuario ha iniciado sesión
-            echo '<button onclick="window.location.href=\'registrar_public.php\'">';
-            echo '<span>';
-            echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path></svg> Crear';
-            echo '</span>';
-            echo '</button>';
-        }
         ?>
     </div>
 </body>
